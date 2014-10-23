5db7e384597d3d58057dc6cf12e63da086443406
X00101856 <X00101856@gmail.com>
2014-10-23 21:41:30 +0100

    added X00101856-grades.sh to repo

---
+++ X00101856-grades.sh
@@ -0,0 +1,30 @@
+#Test Case 1. 
+#Expected Output: Fail
+#Observed Output: Fail
+java grades 49 49
+
+#Test Case 2. 
+#Expected Output: Component Fail
+#Observed Output: Component Fail
+java grades 39 80
+
+#Test Case 3.
+#Expected Output: Component Fail
+#Observed Output: Component Fail
+java grades 80 39
+
+#Test Case 4.
+#Expected Output: Pass
+#Observed Output: Pass
+java grades 60 60
+
+#Test Case 5.
+#Expected Output:Pass with distinction
+#Observed Output: Pass
+java grades 80 80
+
+#Test Case 6.
+#Expected Output: Invalid Input
+#Observed Output: Invalid Input
+java grades -1 101
+
